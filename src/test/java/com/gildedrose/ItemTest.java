2a729cfe87dbc9cc33672a0bef14b4fc9699d802
王增舟 <wangzengzhou@ujumedia.com>
2022-07-23 15:42:04 +0800

    update lib version

--- src/test/java/com/gildedrose/ItemTest.java
+++ src/test/java/com/gildedrose/ItemTest.java
@@ -1,10 +1,10 @@
 package com.gildedrose;
 
 import com.gildedrose.items.BackstagePass;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class ItemTest {
     @Test
