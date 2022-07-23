882136078f7de12358d54ba8e53dd80775e0de2d
王增舟 <wangzengzhou@ujumedia.com>
2022-07-23 15:02:38 +0800

    update lib version

--- src/test/java/com/gildedrose/ItemTest.java
+++ src/test/java/com/gildedrose/ItemTest.java
@@ -1,11 +1,12 @@
 package com.gildedrose;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class ItemTest {
+
     @Test
     public void should_increase_backstage_pass_quality_when_it_gets_close_to_expiration() {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);
