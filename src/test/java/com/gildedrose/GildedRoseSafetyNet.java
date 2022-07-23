aed6b3fa6137e105b9459b3a4059088b05a731eb
王增舟 <wangzengzhou@ujumedia.com>
2022-07-23 15:02:38 +0800

    update lib version

--- src/test/java/com/gildedrose/GildedRoseSafetyNet.java
+++ src/test/java/com/gildedrose/GildedRoseSafetyNet.java
@@ -1,19 +1,20 @@
 package com.gildedrose;
 
 import com.google.common.io.Files;
-import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
 import static com.google.common.base.Charsets.UTF_8;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class GildedRoseSafetyNet {
+
     @Test
     public void should_always_generate_output_identical_to_baseline() throws IOException {
-        String baseline = Files.toString(new File("src/test/baseline.txt"), UTF_8);
+        String baseline = Files.asCharSource(new File("src/test/baseline.txt"), UTF_8).read();
         assertThat(TextTestFixture.generateBaselineOutput(), is(baseline));
     }
 }
