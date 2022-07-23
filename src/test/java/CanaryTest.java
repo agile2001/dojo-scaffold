ba3031dfc6ace711f31c3b9ea9ecadf93d69f674
王增舟 <wangzengzhou@ujumedia.com>
2022-07-23 12:12:04 +0800

    update lib version

--- src/test/java/CanaryTest.java
+++ src/test/java/CanaryTest.java
@@ -1,17 +1,17 @@
 import com.google.common.io.Files;
-import org.junit.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import org.junit.jupiter.api.Test;
 
 import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.collect.Lists.newArrayList;
 import static java.util.stream.Collectors.toList;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
-public class CanaryTest {
+class CanaryTest {
+
     @Test
     public void should_be_able_to_run_a_trivial_test_case() {
         assertThat(1 + 2, is(3));
@@ -23,7 +23,7 @@ public class CanaryTest {
         List<Integer> doubled = numbers.stream().map(x -> x * 2).collect(toList());
         assertThat(doubled, is(newArrayList(2, 4, 6, 8, 10)));
 
-        String src = Files.toString(new File("src/test/java/CanaryTest.java"), UTF_8);
-        assertThat(src.length(), is(954));
+        String src = Files.asCharSource(new File("src/test/java/CanaryTest.java"), UTF_8).read();
+        assertThat(src.length(), is(980));
     }
 }
