61cf856c34fa4650d5faa418073ef2d958a4bf20
王增舟 <wangzengzhou@ujumedia.com>
2022-07-23 16:42:45 +0800

    update lib version

--- src/test/java/com/gildedrose/items/AgedBrieTest.java
+++ src/test/java/com/gildedrose/items/AgedBrieTest.java
@@ -1,9 +1,10 @@
 package com.gildedrose.items;
 
-import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
 
 public class AgedBrieTest {
     @Test
