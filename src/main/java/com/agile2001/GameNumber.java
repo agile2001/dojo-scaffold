7fa0284df1b89a149009bc3ee21ba1789d7a3e29
王增舟 <wangzengzhou@ujumedia.com>
2022-09-28 16:12:57 +0800

    fizz buzz step1

--- src/main/java/com/agile2001/GameNumber.java
+++ src/main/java/com/agile2001/GameNumber.java
@@ -9,19 +9,7 @@ public class GameNumber {
     }
 
     public String execute() {
-        if (isRelatedTo(3) && isRelatedTo(5)) {
-            return "FizzBuzz";
-        }
-        if (isRelatedTo(3)) {
-            return "Fizz";
-        }
-        if (isRelatedTo(5)) {
-            return "Buzz";
-        }
         return String.valueOf(number);
     }
 
-    private boolean isRelatedTo(int x) {
-        return number % x == 0 || String.valueOf(number).contains(String.valueOf(x));
-    }
 }
